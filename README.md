a37ae2f0a22da16ba9857d52035a836997ae1ad5
Vijay Sharma <vijay.sharm@gmail.com>
2014-03-24 12:36:29 -0400

    Committing first version of my resume

---
+++ README.md
@@ -0,0 +1,11 @@
+# RESUME
+## Summary
+This is my resume application. I decided that I dislike creating and maintaining my resumes, so I wrote this simple page which reads my resume from a json file, and then renders it to a web page. Once its been rendered, I can export it to PDF thanks to chrome's 'print-to-pdf' feature. Come tomorrow, when I no longer like the theme of my resume, I can easily just edit a CSS instead of some other weird means of styling my resume (I'm looking at you Office Word, or web-site builders that charge money). 
+
+## Exporting
+You may have to start a web server (try `python -m SimpleHTTPServer 8080` on macs) and then open the page. Then command-P will open a print page in chrome and you can select the print to PDF option and save.
+
+## TODO
++ Need to support footers for page information
++ Make it look slightly better
+
---
+++ resume.js
@@ -0,0 +1,38 @@
+Handlebars.registerHelper('log', function(obj) {
+	console.log(obj);
+});
+
+Handlebars.registerHelper('flat', function(items) {
+	var result = '';
+	for ( var index = 0; index < items.length; index++ ) {
+		var item = items[index].title ? items[index].title : items[index];
+		result += index === 0 ? item : ', ' + item;
+	}
+
+	return result;
+});
+
+function loadTemplateById( id ) {
+	return Handlebars.compile($(id).html());
+};
+
+function loadResume( resume ) {
+	var title = loadTemplateById('#title');
+	var experience = loadTemplateById('#experience');
+	var education = loadTemplateById('#education');
+	var items = loadTemplateById('#items');
+	var flat_items = loadTemplateById('#flat-items');
+
+	var page = $('#main');
+	page.append(title(resume));
+	page.append(experience(resume.professionalexperience));
+	page.append(education(resume.education));
+	page.append(items(resume.awards));
+	page.append(flat_items(resume.languages));
+};
+
+$.ajax({
+	url: 'resume.json',
+	dataType: 'json',
+	success: loadResume
+});
